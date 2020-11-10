e6f6a82943197c91519090cebbadcfeb9152afd1
kaveh fazaeli <kaveh.fazaeli@gmail.com>
2020-11-10 21:19:06 +0330

    Leftist Heap Insert Benchmark with 4 data type

--- leftist/leftist_heap_test.go
+++ leftist/leftist_heap_test.go
@@ -1,6 +1,8 @@
 package leftist
 
 import (
+	"fmt"
+	"math/rand"
 	"sort"
 	"testing"
 
@@ -23,6 +25,73 @@ func TestLeftistHeapInteger(t *testing.T) {
 			t.Fail()
 		}
 	}
+}
+
+func BenchmarkLeftistHeapInsert(b *testing.B) {
+	for _, size := range []int{1, 10, 100, 1000, 10000, 100000, 1000000} {
+		benchmarkLeftistHeapInsertRand(b, size)
+	}
+}
+
+func benchmarkLeftistHeapInsertRand(b *testing.B, size int) {
+	var numbers_rand = make([]int, size)
+	rand.Seed(int64(size % 64))
+	for i := 0; i < size; i++ {
+		numbers_rand[i] = rand.Intn(size)
+	}
+
+	var numbers_rep = make([]int, size)
+	for i := 0; i < size; i++ {
+		numbers_rep[i] = 1
+	}
+
+	var numbers_incr = make([]int, size)
+	for i := 0; i < size; i++ {
+		numbers_incr[i] = i
+	}
+
+	var numbers_decr = make([]int, size)
+	for i := size; i < 0; i-- {
+		numbers_decr[i] = i
+	}
+	heap1 := New().Init()
+	heap2 := New().Init()
+	heap3 := New().Init()
+	heap4 := New().Init()
+
+	b.ResetTimer()
+
+	b.Run(fmt.Sprintf("Insert_Rand_%d_Item", size), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < size; j++ {
+				heap1.Insert(Int(numbers_rand[j]))
+			}
+		}
+	})
+
+	b.Run(fmt.Sprintf("Insert_Rep_%d_Item", size), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < size; j++ {
+				heap2.Insert(Int(numbers_rep[j]))
+			}
+		}
+	})
+
+	b.Run(fmt.Sprintf("Insert_Incr_%d_Item", size), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < size; j++ {
+				heap3.Insert(Int(numbers_incr[j]))
+			}
+		}
+	})
+
+	b.Run(fmt.Sprintf("Insert_decr_%d_Item", size), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < size; j++ {
+				heap4.Insert(Int(numbers_decr[j]))
+			}
+		}
+	})
 }
 
 func TestLeftistHeapString(t *testing.T) {
